8137609ac7a07039f20ad78abda0cb2a230eea66
ricochetcoin <29815783+ricochetcoin@users.noreply.github.com>
2018-01-31 17:46:59 +0200

    Update index.php

--- q/reward/index.php
+++ q/reward/index.php
@@ -1,6 +1,6 @@
 <?php 
 $data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
-$ch = curl_init('http://199.247.6.128:7620/json_rpc');
+$ch = curl_init('http://explorer.ricochet.pro/json_rpc');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
